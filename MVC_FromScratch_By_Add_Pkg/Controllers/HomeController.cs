4d5668cfd431f430e82cb1ecdb55c1bb3439a0b2
Carlo Roncallo <WRoncallo@rubytuesday.com>
2018-01-03 13:51:59 -0500

    Add project files.

---
+++ MVC_FromScratch_By_Add_Pkg/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_FromScratch_By_Add_Pkg
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View("Index", "_LayoutPage1");
+        }
+    }
+}
---
+++ MVC_FromScratch_By_Add_Pkg/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Routing;
+
+namespace MVC_FromScratch_By_Add_Pkg
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
